113918705ad2047740860b1d1cc291901fff12ef
Yacine Bendjebbar <yacine.benjebbar@gmail.com>
2025-01-13 00:54:18 +0100

    fix: added express

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
 import { Bot } from "grammy";
 import { getQuote } from "./utils/quotes";
 import { configDotenv } from "dotenv";
+import express from "express";
 
 configDotenv();
 
 const bot = new Bot(process.env.TOKEN as string);
 
 bot.command("start", (ctx) => {
-  ctx.reply("Welcome home, master!");
-});
-
-bot.command("chat", (ctx) => {
   ctx.reply("Speak thine heart's desires");
 });
 
@@ -23,18 +20,31 @@ bot.command("quote", async (ctx) => {
   }
 });
 
-setInterval(async () => {
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.get("/send-quote", async (req, res) => {
   try {
     const quotes = await getQuote();
     quotes.map((quote) =>
       bot.api.sendMessage(
         process.env.CHAT_ID as string,
-        quote.q + " - " + quote.a
+        `${quote.q} - ${quote.a}`
       )
     );
+    res.status(200).send("Quote sent!");
   } catch (error) {
     console.error(error);
+    res.status(500).send("Failed to send quote.");
   }
-}, 5400000);
+});
 
-bot.start();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  bot.start();
+});
